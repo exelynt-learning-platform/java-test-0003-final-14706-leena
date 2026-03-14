c1da37cf4e69856496ee4512ee8f6414d4e152f4
rathodleena <102647873+rathodleena@users.noreply.github.com>
2026-03-14 12:02:00 +0530

    Added BinaryPattern.java solution
    solution is added

---
+++ BinaryPattern.java
@@ -0,0 +1,22 @@
+public class BinaryPattern {
+  public static void main(String[] args) {
+    int n = 6;
+    for(int i = 1; i <= n; i++) {
+      int num;
+      if(i % 2 == 0) {
+        num = 0;
+      } else {
+        num = 1;
+      }
+      for(int j = 1; j <= i; j++) {
+        System.out.print(num);
+        if(num == 0) {
+          num = 1;
+        } else {
+          num = 0;
+        }
+      }
+      System.out.println();
+    }
+  }
+}
